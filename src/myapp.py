feb0c1650defd18fcdbc6ed1e105ad089ee008f3
tvs_senthil <senthilkumarimuthu@gmail.com>
2023-03-22 17:07:33 +0530

    config.toml is added

---
+++ src/myapp.py
@@ -0,0 +1,22 @@
+from flask import Flask
+import toml
+
+
+#load toml file
+config = toml.load('./config/config.toml')
+# Setting up the app
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    """
+    return project name
+
+    :return: string
+    """
+    return str(config['project']['name'])
+
+
+if __name__ == "__main__":
+    app.run(port=500, debug=True)
